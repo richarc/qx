6ceee841487f874958d83b9033bf41324d93c5de
Craig Richards <3357598+richarc@users.noreply.github.com>
2025-11-26 21:18:17 +0000

    bumped version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Qx.MixProject do
   def project do
     [
       app: :qx,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
